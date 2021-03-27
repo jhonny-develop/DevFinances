e29d88edc141507a9d19233ed4e5d894127d2430
jhonny-develop <jhonnymarques.dev@gmail.com>
2021-03-27 16:36:30 -0300

    estilos do cabeçalho

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const Header = styled.header`
+  background: #2d4a22;
+  padding: 2rem 0 10rem;
+  text-align: center;
+`
+export const Logo = styled.h1`
+  color: #fff;
+  font-weight: 100;
+`
